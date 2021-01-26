b548fcabccc728402798002a65c16b59572faa84
Blake Blackwell <blakewell@gmail.com>
2021-01-26 14:49:01 +0000

    adding device info api calls

--- device-api/src/index.js
+++ device-api/src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const si = require("systeminformation")
+
 const server = http.createServer(app);
 const port = 8089;
 
-app.get('/', (req, res) => {
- res.status(200).send(`Hello World! Our IOT Device HTTP server is running on port ${port}`);
+app.get('/device/cpu', (req, res) => {
+ si.cpu().then(data => res.status(200).send(data));
 });
+
+app.get('/device/mem', (req,res) => {
+ si.mem().then(data => res.status(200).send(data));
+})
+
+app.get('/device/os', (req,res) => {
+ si.osInfo().then(data => res.status(200).send(data));
+})
 
 server.listen(port, () => {
  console.log(`Server running at port ${port}`);
